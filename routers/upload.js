4ba80fc474742a56eb71e276153e2ce535d6e3ae
laoyoufen <>
2019-01-30 16:47:50 +0800

    feat: 分片上传功能

--- routers/upload.js
+++ routers/upload.js
@@ -1,13 +1,44 @@
 const router = require('koa-router')();
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
-router.post('/upload', (ctx) => {
-  const file = ctx.request.files.file;
-  const readStream = fs.createReadStream(file.path)
-  const writeStream = fs.createWriteStream(path.resolve('upload', file.name))
-  readStream.pipe(writeStream)
+router.get('/check/md5', async(ctx) => {
+  const fileMd5 = ctx.query.m;
+  fs.readdirSync(path.resolve('temp'))
+})
+
+router.post('/upload', async (ctx) => {
+  const fileMd5 = ctx.request.body.fileMd5;
+  const tempbuffer = fs.readFileSync(ctx.request.files.chunk.path);
+
+  await fs.ensureDir(path.resolve('temp', fileMd5))
+
+  fs.writeFileSync(path.resolve('temp', `${ctx.request.body.fileMd5}`, `${ctx.request.body.num}`), tempbuffer)
+
+  // const writeStream = fs.createWriteStream(path.resolve('temp', `${ctx.request.body.fileMd5}`, `${ctx.request.body.num}`))
+  // const readerStream = fs.createReadStream(ctx.request.files.chunk.path)
+  // readerStream.pipe(writeStream)
   ctx.body = true
+})
+
+router.get('/merge', (ctx) => {
+  const fileMd5 = ctx.query.m;
+  const fileName = ctx.query.name;
+  const files = fs.readdirSync(path.resolve('temp', fileMd5))
+  const buffers = [];
+  files.forEach((name) => {
+    const buffer = fs.readFileSync(path.resolve('temp', fileMd5, name));
+    buffers.push(buffer);
+  })
+  const concatBuffer = Buffer.concat(buffers);
+  fs.writeFileSync(path.resolve('upload', fileName), concatBuffer);
+
+  async function remove(folder) {
+    await fs.remove(folder)
+  }
+  remove(path.resolve('temp', fileMd5))
+  ctx.body = true
+
 })
 
 module.exports = router;
